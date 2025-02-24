b8a33501a6626ba9f46ead412f460488638eb2d2
Tanmay Naik <68956673+tanmaynaik@users.noreply.github.com>
2025-02-24 22:33:06 +0530

    Add files via upload

--- Employee Project/LoginEmployee.java
+++ Employee Project/LoginEmployee.java
@@ -1,6 +1,12 @@
 package com.setuptable;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,17 +14,33 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("")
-public class LoginEmployee extends HttpServlet{
-	
+@SuppressWarnings("serial")
+@WebServlet("/loginemp")
+public class LoginEmployee extends HttpServlet {
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/batch204", "root", "saitama");
+
+			PreparedStatement ps = c.prepareStatement("SELECT * FROM employee WHERE email = ? AND password = ?");
+			ps.setString(1, email);
+			ps.setString(2, password);
+			ps.executeQuery();
 			
 			
-			
+
+			PrintWriter out = resp.getWriter();
+			out.print("<script>alert('login successful')</script>");
+
+
+			c.close();
+
 		} catch (Exception e) {
 			System.out.println("Wrong get out");
 		}
