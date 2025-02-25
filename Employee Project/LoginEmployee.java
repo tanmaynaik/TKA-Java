c92d64c6f29056e9ac698196604edaaf26be380c
Tanmay Naik <68956673+tanmaynaik@users.noreply.github.com>
2025-02-26 00:08:44 +0530

    Add files via upload

--- Employee Project/LoginEmployee.java
+++ Employee Project/LoginEmployee.java
@@ -7,11 +7,13 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @SuppressWarnings("serial")
 @WebServlet("/loginemp")
@@ -22,6 +24,8 @@ public class LoginEmployee extends HttpServlet {
 
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
+		
+		
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -33,10 +37,30 @@ public class LoginEmployee extends HttpServlet {
 			ps.executeQuery();
 			
 			PrintWriter out = resp.getWriter();
+			
 
 			ResultSet rs = ps.executeQuery();
 			if(rs.next()) {
 				out.print("<script>alert('login successful')</script>");
+//				RequestDispatcher rd = req.getRequestDispatcher("ProfilePage.jsp");
+//				rd.include(req, resp);
+				
+				HttpSession hs = req.getSession();
+				hs.setAttribute("fname", rs.getString(2));
+				hs.setAttribute("lname", rs.getString(3));
+				hs.setAttribute("salary", rs.getString(4));
+				hs.setAttribute("role", rs.getString(5));
+				hs.setAttribute("join_date", rs.getString(6));
+				hs.setAttribute("experience", rs.getString(7));
+				hs.setAttribute("city", rs.getString(8));
+				hs.setAttribute("country", rs.getString(9));
+				hs.setAttribute("email", rs.getString(10));
+				hs.setAttribute("mobile", rs.getString(11));
+				hs.setAttribute("age", rs.getString(12));
+				hs.setAttribute("marital_status", rs.getString(13));
+				hs.setAttribute("dob", rs.getString(14));
+				resp.sendRedirect("ProfilePage.jsp");
+				
 			}
 			else {
 				out.print("<script>alert('login failed')</script>");
