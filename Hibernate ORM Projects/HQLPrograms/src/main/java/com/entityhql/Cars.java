a439e79d06046c276014e7febf047e5afd5c10c8
Tanmay Naik <68956673+tanmaynaik@users.noreply.github.com>
2025-03-31 14:03:18 +0530

    Add files via upload

---
+++ Hibernate ORM Projects/HQLPrograms/src/main/java/com/entityhql/Cars.java
@@ -0,0 +1,13 @@
+package com.entityhql;
+
+import org.hibernate.cfg.Configuration;
+
+public class Cars {
+	
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure();
+		cfg.addAnnotatedClass()
+	}
+
+}
